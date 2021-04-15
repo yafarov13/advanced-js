510630653d456fb2e18f6c21f97b30236d4497bd
Ildar <yafarov13@gmail.com>
2021-04-15 21:11:38 +0300

    Homework6

---
+++ js/SearchComponent.js
@@ -0,0 +1,12 @@
+Vue.component('search', {
+    template: `
+        <form action="#" class="search-form" @submit.prevent="$root.filter">
+            <input type="text" class="search-field" v-model='$root.userSearch'>
+            <button type="submit" class="btn-search">
+            <i class="fas fa-search"></i>
+            </button>
+        </form>
+    `
+});
+
+
